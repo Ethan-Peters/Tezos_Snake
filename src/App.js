93fd174baab6a189921b592eedb89f508504bab7
Ethan-Peters <ethanconradpeters@gmail.com>
2021-12-08 00:30:27 -0800

    Snake can eat food and collide with itself

--- src/App.js
+++ src/App.js
@@ -51,6 +51,16 @@ class App extends React.Component {
         snakeDots: dots
       })
     this.checkIfOutOfBounds();
+    if(this.checkIfFood()){
+      this.setState({
+        food: this.getNewFood()
+      });
+      this.enlargeSnake();
+      if(this.state.speed > 50){
+        this.setState({speed: this.state.speed -10});
+      }
+    }
+    this.checkIfColliding();
   }
   onKeyDown = (e) => {
     e = e || window.event;
@@ -80,12 +90,55 @@ class App extends React.Component {
       this.onGameOver();
     }
   }
+  checkIfFood=()=>{
+    let head = this.state.snakeDots;
+    let food = this.state.food;
+    if(food[0] == head[head.length-1][0] && food[1] == head[head.length-1][1]){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 
   onGameOver=()=>{
     alert(`GAME OVER! Your score was ${this.state.snakeDots.length}`);
     this.setState(initialState);
   }
 
+  enlargeSnake=()=>{
+    let newSnake = this.state.snakeDots;
+    newSnake.unshift([]);
+    this.setState({
+      snakeDots: newSnake
+    });
+  }
+
+  checkIfColliding=()=>{
+    let head = this.state.snakeDots[this.state.snakeDots.length-1];
+    let snake = this.state.snakeDots;
+    for(let x = 0; x < snake.length -1; x++){
+      if(snake[x][0] ==head[0] && snake[x][1] == head[1]){
+        this.onGameOver();
+      }
+    }
+  }
+  getNewFood=()=>{
+    let flag = true;
+    let coordinates = getRandomCoordinates();
+    let snake = this.state.snakeDots;
+    for(let x = 0; x < snake.length; x++){
+      if(snake[x][0] ==coordinates[0] && snake[x][1] == coordinates[1]){
+        flag = false;
+      }
+    }
+    if(flag){
+      return coordinates;
+    }
+    else{
+      this.getNewFood();
+    }
+  }
   render() {
     return (
       <div className = "game-area">
